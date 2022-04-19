6b919f98d4f608fd74c8630764b125dcd8865bf6
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2022-04-19 15:49:18 -0400

    Initial Commit

---
+++ Helpnet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Helpnet
+//
+//  Created by Subomi Popoola on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
